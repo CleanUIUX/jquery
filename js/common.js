00f0dbc8f4bdf1ce0befe6abb41d4714bdc887fb
channy <ox043440@gmail.com>
2023-11-06 18:31:30 +0900

    231106

--- js/common.js
+++ js/common.js
@@ -37,10 +37,18 @@ var ui = {
         },
         //  tabs
         tabs: {
-            evt : function fnMove(seq){
-                var offset = $('#div' + seq).offset();
-                $('html, body').animate({scrollTop : offset.top}, 400);
+            evt : function(){
+                
                 // 클릭 시 스크롤 이동
+                $(".main_tab li").on("click", function(event){
+                    $('html, body').animate({
+                        screenTop: $($.attr(this, 'href').offset().top)
+                    }, 500);
+                    return false;
+                    
+                    // console.log(this);
+                })
+
                 // var _this = this;
                 // $(document).on('click', '.main_tab > li .bt', function(){
                 //     var id = $(this).closest("li").data("btn-sid");
@@ -48,7 +56,9 @@ var ui = {
                 // });
             }
             // goto: function(id){
+            //     console.log(id);
             //     var test = $("[data-sid="+id+"]").offset().top;
+            //     var navHt = $(".main_tab:visible").outerHeight() || 0;
                 
             // }
         }
@@ -58,3 +68,65 @@ var ui = {
 $(function(){
     ui.init();
 });
+
+
+
+// 
+ // a클릭시 부드럽게 이동
+ $('a').click(function() {
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+});
+
+// 퀵메뉴
+$('.quick_menu li a').click(function() {
+    // 버튼 hover 이벤트
+    return false
+    $(this).parent().addClass('on');
+    $(this).parent().siblings().removeClass('on');
+});
+// target 위치 표시와, 이동  
+var sections = $('.target'),
+    nav = $('.quick_menu'),
+    nav_height = nav.outerHeight();
+
+$(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+        var top = $(this).offset().top - nav_height,
+            bottom = top + $(this).outerHeight();
+
+        if (cur_pos >= top && cur_pos <= bottom) {
+            nav.find('a').parent().removeClass('on');
+            sections.removeClass('active');
+
+            $(this).parent().addClass('on');
+            nav.find('a[href="#' + $(this).attr('id') + '"]').parent().addClass('on');
+        }
+    });
+});
+
+nav.find('a').on('click', function() {
+    var $el = $(this),
+        id = $el.attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(id).offset().top 
+    }, 500);
+
+    return false;
+});
+
+
+// 원하는 위치에서 스크롤 이벤트
+$(window).on('scroll', function() {
+    if ($(window).scrollTop() > 520) {
+        $('.quick_menu').addClass("fixed");
+    } else {
+        $('.quick_menu').removeClass("fixed");
+    }
+})
+// 
