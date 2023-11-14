16e427ca4d72eeaf6e0231f61376e00ebee617a2
channy <ox043440@gmail.com>
2023-11-14 18:15:29 +0900

    231114

--- js/common.js
+++ js/common.js
@@ -52,5 +52,27 @@ $(document).ready(function(){
         for(i = 0; i < $index; i++){
             $total += $con.eq(i).outerHeight(true);
         };
+
+        var $offset = $con.eq($index).offset().top;
+
+        if($index == 0){
+            $('.sub_menu').stop().delay(100).animate({scrollTop:0}, 400);
+        }
+        // else{
+        //     $('.sub_menu').stop().delay(100).animate({scrollTop:$total+20}, 400);
+        // }
+        e.preventDefault();
     });
+
+    $('.sub_menu').on('scroll', function(){
+        var $this = $(this);
+        var $con = $this.find('> ul > li');
+        var $nav = $('.menu_list li');
+        var $subTop = $this.scrollTop();
+        var $len = $con.length;
+
+        for(i = 0; i < $len; i++){
+            
+        }
+    })
 });
