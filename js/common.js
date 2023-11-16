5bcc662d65c8e8622b4306c642bc1fce20908c8d
channy <ox043440@gmail.com>
2023-11-16 18:10:20 +0900

    231116

--- js/common.js
+++ js/common.js
@@ -6,34 +6,47 @@ var ui = {
     common: {
         bindEvents : function(){ 
             // 인풋 텍스트 입력 시 삭제버튼 생성
-            $('.input_box').find('.btn_del').hide();
-            $('.input_info').on('focus change keyup paste', function(e){
+            $('.type1 > .input_box').find('.btn_del').hide();
+            $('.type1 > .input_info').on('focus change keyup paste', function(e){
                 if($(this).val() == ''){
-                    $(e.target).parents('.input_box').find('.btn_del').hide();
+                    $(e.target).parents('.type1 > .input_box').find('.btn_del').hide();
                 }else{
-                    $(e.target).parents('.input_box').find('.btn_del').show();
+                    $(e.target).parents('.type1 > .input_box').find('.btn_del').show();
                 }
                 // 글자수 제한
                 const inputLength = $(this).val().length;
                 if($(this).val().length > 12){
                     $(this).val($(this).val().substring(0, 13));
 
-                    $('.input_btn').addClass("on");
+                    $('.type1 > .input_btn').addClass("on");
                 }
                 // 하이픈 적용
                 $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") );
             });
             // 삭제 버튼 클릭 시 value 값 삭제 및 포커스
-            $('.input_box .btn_del').on('click', function(e){
+            $('.type1 > .input_box .btn_del').on('click', function(e){
                 e.preventDefault();
 
-                const $inputDel = $(e.target).parents('.input_box').find('input');
+                const $inputDel = $(e.target).parents('.type1 > .input_box').find('input');
 
                 if(!$inputDel.prop('readonly')){
                     $inputDel.val('').focus();
                 };
                 $(this).hide();
-            });       
+            });     
+            
+            // 클릭 시 info on
+            $('.btn_search').on('click', function(){
+                if($('.type2 > .input_wrap').hasClass("on")){
+                    $('.type2 > .input_wrap').slideUp(500, function(){
+                        $('.type2 > .input_wrap').removeClass("on");
+                    })
+                }else{
+                    $('.type2 > .input_wrap').slideDown(500, function(){
+                        $('.type2 > .input_wrap').addClass("on");
+                    });
+                };
+            });
         }
     }
 }
@@ -58,9 +71,9 @@ $(document).ready(function(){
         if($index == 0){
             $('.sub_menu').stop().delay(100).animate({scrollTop:0}, 400);
         }
-        // else{
-        //     $('.sub_menu').stop().delay(100).animate({scrollTop:$total+20}, 400);
-        // }
+        else{
+            $('.sub_menu').stop().delay(100).animate({scrollTop:$total+20}, 400);
+        }
         e.preventDefault();
     });
 
@@ -72,7 +85,10 @@ $(document).ready(function(){
         var $len = $con.length;
 
         for(i = 0; i < $len; i++){
-            
+            if($con.eq(i).offset().top){
+                $nav.removeClass("on");
+                $nav.eq(i).addClass("on");
+            }
         }
     })
 });
