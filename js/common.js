100247e8a86d0eedde4ad5b402644bfa89340f5d
channy <ox043440@gmail.com>
2023-11-20 18:30:50 +0900

    231120

--- js/common.js
+++ js/common.js
@@ -6,28 +6,31 @@ var ui = {
     common: {
         bindEvents : function(){ 
             // 인풋 텍스트 입력 시 삭제버튼 생성
-            $('.type1 > .input_box').find('.btn_del').hide();
-            $('.type1 > .input_info').on('focus change keyup paste', function(e){
+            $('.input_box').find('.btn_del').hide();
+            $('.input_info.phoneNumber').on('focus change keyup paste', function(e){
                 if($(this).val() == ''){
-                    $(e.target).parents('.type1 > .input_box').find('.btn_del').hide();
+                    $(e.target).parents('.input_box').find('.btn_del').hide();
                 }else{
-                    $(e.target).parents('.type1 > .input_box').find('.btn_del').show();
+                    $(e.target).parents('.input_box').find('.btn_del').show();
                 }
                 // 글자수 제한
                 const inputLength = $(this).val().length;
+                console.log(this);
                 if($(this).val().length > 12){
                     $(this).val($(this).val().substring(0, 13));
 
-                    $('.type1 > .input_btn').addClass("on");
+                    $('.input_btn').addClass("on");
+                }else{
+                    $('.input_btn').removeClass("on");
                 }
                 // 하이픈 적용
                 $(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") );
             });
             // 삭제 버튼 클릭 시 value 값 삭제 및 포커스
-            $('.type1 > .input_box .btn_del').on('click', function(e){
+            $('.input_box .btn_del').on('click', function(e){
                 e.preventDefault();
 
-                const $inputDel = $(e.target).parents('.type1 > .input_box').find('input');
+                const $inputDel = $(e.target).parents('.input_box').find('input');
 
                 if(!$inputDel.prop('readonly')){
                     $inputDel.val('').focus();
